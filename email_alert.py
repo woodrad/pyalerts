a14eb5c8e43c263ee04a79de54c91ff3113481de
woodrad <mwoodyard+github@5lbs.org>
2017-01-21 13:02:54 -0600

    Send an email alert if disk use is over a given threshold.

--- email_alert.py
+++ email_alert.py
@@ -1,18 +1,39 @@
-# Import smtplib for the actual sending function.
+#!/usr/bin/env python3
+from subprocess import check_output
+import re
 import smtplib
-
-# Import the needed modules for email formatting.
 from email.mime.text import MIMEText
 
-# Create a text/plain formatted email.
-msg = MIMEText('Rootfs usage over alert threshold')
 
-# Add SMTP elements needed for send_message later.
-msg['Subject'] = 'Rootfs usage alert'
-msg['From'] = 'jersey-server@dephekt.net'
-msg['To'] = 'cellphone@txt.att.net'
+def high_disk_use(threshold=90):
+    """ Checks to see whether disk use is above the given threshold.
+    Args:
+        threshold: the disk utilization threshold to trigger an alert.
+    Returns:
+        A boolean indicating whether disk usage is over `threshold`.
+        True when disk usage is over `threshold`, False otherwise.
+    """
+    df_output = check_output(["df", "/"])
+    find_usage = re.findall(b"(\d+)%", df_output)
+    disk_usage_percent = int(find_usage[0])
 
-# Send the message via our own SMTP server
-s = smtplib.SMTP('localhost')
-s.send_message(msg)
-s.quit()
+    if disk_usage_percent > threshold:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    if high_disk_use():
+        # Create a text/plain formatted email.
+        msg = MIMEText('Rootfs usage over alert threshold')
+
+        # Add SMTP elements needed for send_message later.
+        msg['Subject'] = 'Rootfs usage alert'
+        msg['From'] = 'jersey-server@dephekt.net'
+        msg['To'] = 'cellphone@txt.att.net'
+
+        # Send the message via our own SMTP server
+        s = smtplib.SMTP('localhost')
+        s.send_message(msg)
+        s.quit()
